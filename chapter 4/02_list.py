6962c1a8b8deb57ffe18a871567ba912ca08c167
Mohammed Mustafa Ali Khan <mohammedmustafaalikhan9@gmail.com>
2025-10-03 19:33:02 +0530

    Add files via upload

---
+++ chapter 4/02_list.py
@@ -0,0 +1,14 @@
+friends = ["Apple","Orange",5,345.06,False,"Aakash","Raheem"]
+print(friends)
+friends.append("Mustafa")
+print(friends)
+
+l1 = [1,34,62,2,6,11]
+#l1.sort()
+#l1.reverse()
+#l1.insert(3, 333333)
+#value =l1.pop(3)
+# print(value)
+# print(l1)
+l1.remove(34)
+print(l1)
---
+++ chapter 4/04_tuple.py
@@ -0,0 +1,8 @@
+a= (1,2,3424,"Monday", "Tuesday", "Thursday", "Friday")
+print(a)
+
+no = a.count(2)
+print(no)
+
+i = a.index(3424)
+print(i)
