5c15aedf350eb615ffee910a05608cb63074d169
Mohammed Mustafa Ali Khan <mohammedmustafaalikhan9@gmail.com>
2025-10-08 22:21:08 +0530

    Add files via upload

---
+++ chapter 5/01_dict.py
@@ -0,0 +1,11 @@
+marks ={
+    "Harry":100,
+    "Shahrukh":56,
+    "Ramesh":23,
+}
+
+print(marks,type(marks))
+print (marks["Harry"])
+
+d={0:1,1:"Harry"}
+print (len(d))#length
---
+++ chapter 5/02_dict_methods.py
@@ -0,0 +1,16 @@
+marks ={
+    "Harry":100,
+    "Shahrukh":56,
+    "Ramesh":23,
+    0:"Harry"
+}
+
+#print(marks.items())
+#print(marks.keys())
+#print(marks.values())
+#marks.update({"Harry":99, "Renuka":100})
+#print(marks)
+
+print(marks.get("Harry2"))
+print(marks["Harry2"])
+
