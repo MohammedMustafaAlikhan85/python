ef5e6f62b333df01c84dfa5184fda5d1a46ece60
Mohammed Mustafa Ali Khan <mohammedmustafaalikhan9@gmail.com>
2025-10-27 21:31:21 +0530

    Implement function to remove word from list
    Fix the removal of specified word from list items.

--- chapter 8 ps/07_problem.py
+++ chapter 8 ps/07_problem.py
@@ -1,1 +1,12 @@
 
+def rem(l,word):
+    n =[]
+    for item in l:
+        if not (item == word):
+            n. append(item.strip(word))
+    return n
+
+
+l = ["Harry","Rohan","Jetha"]
+
+print(rem(l,"an"))
