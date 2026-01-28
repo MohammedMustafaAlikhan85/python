f59aa4e8208cbbe2276008f0f7aa5ac0b866b44f
Mohammed Mustafa Ali Khan <mohammedmustafaalikhan9@gmail.com>
2026-01-28 23:33:12 +0530

    Update and rename 02.py to instance_vs_classs_attr.py

---
+++ chapter 10/instance_vs_classs_attr.py
@@ -0,0 +1,9 @@
+
+class Employee :
+    language ="Python"#this is a class attribute
+    salary = 1200000
+
+
+harry = Employee()
+harry.language="JavaScript"#this is an instance attribute
+print(harry.language,harry.salary)
