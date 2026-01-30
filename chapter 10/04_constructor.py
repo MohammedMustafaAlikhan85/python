36858fb7e2f51d2904d82fdc875f945cacb647f8
Mohammed Mustafa Ali Khan <mohammedmustafaalikhan9@gmail.com>
2026-01-30 23:40:10 +0530

    Create 04_constructor.py

---
+++ chapter 10/04_constructor.py
@@ -0,0 +1,21 @@
+lass Employee :
+    language ="Python"#this is a class attribute
+    salary = 1200000
+
+    def __init__(self,name,salary,language): # dunder method,which is automatically called
+        self.name = name
+        self.salary = salary
+        self.language = language
+        print("I am creating an object")
+
+    def getinfo(self):
+        print(f"The language is{self.language}. The salary is {self.salary}")
+
+    @staticmethod
+    def greet ():
+        print("Good morning")
+
+
+harry = Employee("Harry",130000,"Javascript")
+#harry.name = "Harry"
+print(harry.name,harry.salary,harry.language)
